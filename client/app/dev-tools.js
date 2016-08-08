4b2c8754d9bd68b7fb32f06a214b3e1f1e8e858c
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-08-08 13:46:31 -0700

    moar dev-tools

---
+++ client/app/dev-tools.js
@@ -0,0 +1,15 @@
+import { createDevTools } from 'redux-devtools';
+import LogMonitor from 'redux-devtools-log-monitor';
+import DockMonitor from 'redux-devtools-dock-monitor';
+
+import React from 'react';
+
+const DevTools = createDevTools(
+  <DockMonitor toggleVisibilityKey='ctrl-h'
+                changePositionKey='ctrl-q'
+                defaultIsVisible={true}>
+    <LogMonitor theme='tomorrow' />
+  </DockMonitor>
+);
+
+export default DevTools;
